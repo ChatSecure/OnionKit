6eec8d5af96c5e80080054b6a8d81148f04f0f6c
Chris Ballinger <chrisballinger@gmail.com>
2013-11-19 14:06:01 -0800

    Add basic example

---
+++ OnionKitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OnionKitTest
+//
+//  Created by Christopher Ballinger on 11/19/13.
+//  Copyright (c) 2013 ChatSecure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TORAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TORAppDelegate class]));
+    }
+}
