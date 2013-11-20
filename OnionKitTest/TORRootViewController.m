4ed03bcce0d29f703f357cb16f69efd79988670a
Chris Ballinger <chrisballinger@gmail.com>
2013-11-20 04:15:54 -0800

    Wow SOCKS proxying works

--- OnionKitTest/TORRootViewController.m
+++ OnionKitTest/TORRootViewController.m
@@ -20,8 +20,8 @@ NSString * const DISCONNECT_STRING = @"Disconnect";
 NSString * const CANNOT_RECONNECT_STRING = @"Cannot Reconnect";
 NSString * const TEST_STRING = @"Test";
 
-NSString * const kTorCheckHost = @"check.torproject.org";
-uint16_t const kTorCheckPort = 443;
+NSString * const kTorCheckHost = @"wtfismyip.com";
+uint16_t const kTorCheckPort = 80;
 
 @interface TORRootViewController ()
 @property (nonatomic, strong) GCDAsyncProxySocket *socket;
@@ -138,7 +138,11 @@ uint16_t const kTorCheckPort = 443;
 
 - (void) socket:(GCDAsyncSocket *)sock didConnectToHost:(NSString *)host port:(uint16_t)port {
     NSLog(@"%@ connected to %@ on port %d", sock, host, port);
-    [sock startTLS:nil];
+    //[sock startTLS:nil];
+    NSString *requestString = [NSString stringWithFormat:@"GET /json HTTP/1.1\r\nhost: %@\r\n\r\n", kTorCheckHost];
+    NSData *data = [requestString dataUsingEncoding:NSUTF8StringEncoding];
+    [sock readDataWithTimeout:-1 tag:1];
+    [sock writeData:data withTimeout:-1 tag:0];
 }
 
 - (void)socketDidDisconnect:(GCDAsyncSocket *)sock withError:(NSError *)err {
@@ -147,10 +151,7 @@ uint16_t const kTorCheckPort = 443;
 
 - (void) socketDidSecure:(GCDAsyncSocket *)sock {
     NSLog(@"socket secured: %@", sock);
-    NSString *requestString = [NSString stringWithFormat:@"GET / HTTP/1.1\r\nhost: %@\r\n\r\n", kTorCheckHost];
-    NSData *data = [requestString dataUsingEncoding:NSUTF8StringEncoding];
-    [sock readDataWithTimeout:-1 tag:1];
-    [sock writeData:data withTimeout:-1 tag:0];
+
 }
 
 - (void) socketDidCloseReadStream:(GCDAsyncSocket *)sock {
